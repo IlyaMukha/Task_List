4801d8584376e9c2d7a4a19d258f28ef0cf3d7cd
Ilya Mukha <ilya06010011599@gmail.com>
2021-04-12 16:20:53 +0300

    Добавьте файлы проекта.

---
+++ Task_List/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task_List
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string path = @"C:\Temp\ts.txt";
+        List<Tasks> list = new List<Tasks>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Submit_Click(object sender, RoutedEventArgs e)
+        {
+            list.Add(new Tasks(list.Count + 1, list.Count, TextOfTask.Text));
+            tasksGrid.ItemsSource = list;
+            tasksGrid.Items.Refresh();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(path, false, System.Text.Encoding.Default))
+                {
+                    sw.WriteLine(TextOfTask.Text);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ Task_List/Tasks.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_List
+{
+   public class Tasks
+    {
+        public int Id {get; set;}
+        public int status { get; set; }
+        public string textOfTask { get; set; }
+
+        public Tasks(int Id, int status, string textOfTask)
+        {
+            this.Id = Id;
+            this.status = status;
+            this.textOfTask = textOfTask;
+        }
+    }
+}
